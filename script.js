61300ad608cbc912f7b2732f2d21b8ddb122d4d2
itteam2021 <104797659+itteam2021@users.noreply.github.com>
2022-05-07 22:38:05 +0200

    final

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function myFunction() {
     alert("Thanks For joined our newsletter program\n We have sent you a confirmation E-mail");
   }
   function myiFunction() {
-    alert("Thanks for choosing us \n the Product will be with you as soon as we can ");
+    alert("Thanks for choosing us \n the Product will be with you as soon as we can Please Complete Your information");
   }
